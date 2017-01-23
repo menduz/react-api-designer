d06705b9ad45c09a557b034fdf1210e200a167fa
Juan Longo <jlongo@tekgenesis.com>
2017-01-23 13:41:53 -0300

    DESIGNER-280 added md preview support

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -3,12 +3,13 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {ReactConsole} from '../console'
 import JSONTree from 'react-json-tree'
+import ReactMarkdown from 'react-markdown'
 import './Preview.css'
 
 class Preview extends Component {
 
   render() {
-    const {parsedObject, language} = this.props
+    const {parsedObject, language, text} = this.props
 
     if (!parsedObject || !language.id) return <div className="Preview No-preview">No preview</div>
 
@@ -16,6 +17,8 @@ class Preview extends Component {
       case 'oas':
       case 'raml':
         return <ReactConsole raml={parsedObject}/>
+      case 'md':
+        return <ReactMarkdown source={text}/>
       default:
         return <JSONTree data={parsedObject} theme={{base00:'#000000'}} hideRoot={true}/>
     }
@@ -25,6 +28,7 @@ class Preview extends Component {
 const mapStateToProps = state => {
   const {editor} = state
   return {
+    text: editor.text,
     parsedObject: editor.parsedObject,
     language: editor.language
   }
