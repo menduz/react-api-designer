5296bca685388a2808f0ad29c106ccf5e189e225
Carolina Wright <cwright@tekgenesis.com>
2017-01-30 11:01:50 -0300

    create menu for save, import and export

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -3,11 +3,18 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
+import ContextMenu from '@mulesoft/anypoint-components/lib/ContextMenu'
+import Icon from '@mulesoft/anypoint-icons/lib/Icon'
+
 import NewFolderModalContainer from '../modal/new-folder/NewFolderModalContainer'
 import NewFileModalContainer from '../modal/new-file/NewFileModalContainer'
+import ImportModalContainer from '../modal/import/ImportModalContainer'
+import ExportModalContainer from '../modal/export/ExportModalContainer'
 
 import {openNewFolderDialog} from '../modal/new-folder/NewFolderActions'
 import {openNewFileDialog} from '../modal/new-file/NewFileActions'
+import {openImportDialog} from '../modal/import/ImportActions'
+import {openExportDialog} from '../modal/export/ExportActions'
 
 import './Menu.css'
 import {saveCurrentFile} from "../editor/actions"
@@ -17,11 +24,26 @@ class Menu extends React.Component {
     let {
       showNewFolderDialog,
       showNewFileDialog,
+      showImportDialog,
+      showExportDialog,
       saveFile
     } = this.props
+
+    const options = [
+      {label: 'Save', onClick: saveFile},
+      {label: 'Export', onClick: showExportDialog},
+      {label: 'Import', onClick: showImportDialog},
+    ]
+
     return (
       <div className="menu">
-        <button className="save-file-button" onClick={saveFile}/>
+        <ContextMenu className="context-menu" options={options} triggerOn={['click']}>
+          <Icon className="context-menu-icon" name="contextmenu"/>
+        </ContextMenu>
+
+        <ImportModalContainer/>
+        <ExportModalContainer/>
+
         <button className="new-file-button" onClick={showNewFileDialog}/>
         <NewFileModalContainer/>
 
@@ -42,6 +64,8 @@ const mapDispatch = (dispatch) => {
   return {
     showNewFolderDialog: () => dispatch(openNewFolderDialog()),
     showNewFileDialog: () => dispatch(openNewFileDialog()),
+    showImportDialog: () => dispatch(openImportDialog()),
+    showExportDialog: () => dispatch(openExportDialog()),
     saveFile: () => dispatch(saveCurrentFile())
   }
 }
