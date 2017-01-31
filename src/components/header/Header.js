39031ac04d45fdc5543fc650950cf800ca81189e
Juan Longo <jlongo@tekgenesis.com>
2017-01-31 11:11:55 -0300

    DESIGNER-293 Header styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,29 @@
-//@flow
+// @flow
 
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import Spinner from '@mulesoft/anypoint-components/lib/Spinner'
-import './Header.css'
+import exchangeIcon from './assets/PublishExchangeIcon.svg'
+import Icon from '@mulesoft/anypoint-icons/lib/Icon'
+import './Header.css';
 
 class Header extends Component {
 
   render() {
-    const {isParsing} = this.props
+    const {isParsing, projectName} = this.props
     return (
       <div className="App-header">
-        <h2>Api Designer</h2>
-        {isParsing ? <div className="Spinner-parser"><Spinner size="s"/></div> : null}
+        <div className="Left-header">
+          <Icon name="api-designer-color" size={38}/>
+          <h2>{projectName ? projectName : 'API designer'}</h2>
+        </div>
+        <div className="Right-header">
+          {isParsing ? <div className="Spinner-parser"><Spinner size="s"/></div> : null}
+          <a><img src={exchangeIcon} height="20px"/></a>
+          <span className="Divider"/>
+          <Icon name="support-small" size={19} fill={"white"}/>
+          <Icon name="contextmenu" size={19} fill={"white"}/>
+        </div>
       </div>
     )
   }
