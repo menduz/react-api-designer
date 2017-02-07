422308f48837a8b12e7aebeb01556fcbdaa80265
Lecko <lecko@tekgenesis.com>
2017-02-07 14:50:40 -0300

    Modifying raml baseUri content. to make work angular console. Working on DESIGNER-153

--- src/components/mock/actions.js
+++ src/components/mock/actions.js
@@ -1,6 +1,7 @@
 import MockingService from './service/mocking-service'
 import MockingServiceClient from './service/mocking-service-client'
-
+import {updateCurrentFile} from '../../components/editor/actions'
+import {getCurrentFileContent} from '../../repository-redux/selectors'
 
 export const START_MOCK = 'DESIGNER/MOCK/START_MOCK'
 export const MOCK_STARTED = 'DESIGNER/MOCK/MOCK_STARTED'
@@ -34,12 +35,13 @@ export const createMock = () => (dispatch, getState) => {
   if (!getState().mock.isUp && !getState().mock.isStarting) {
     dispatch(startMock)
     const mock = new MockingService(new MockingServiceClient())
-    //@@TODO GET editor.text from a function!!
-    const ramlContent = getState().editor.text
+    const ramlContent = getCurrentFileContent(getState())()
+    //@@TODO GET editor.parsedObject from a function!!
     const jsonObject = getState().editor.parsedObject
 
     mock.createMock(ramlContent, jsonObject).then(res => {
       dispatch(mockStarted(res.id, res.manageKey, res.baseUri, res.manageUri))
+      addMockBaseUri(ramlContent, res.baseUri)(dispatch, getState)
     }).catch(err => {
       console.log(err)
       dispatch(stopMock)
@@ -48,12 +50,44 @@ export const createMock = () => (dispatch, getState) => {
   }
 }
 
+export const updateMock = (getState) => {
+  const state = getState().mock;
+  if (state.isUp) {
+    const mock = new MockingService(new MockingServiceClient())
+    const ramlContent = getCurrentFileContent(getState())()
+    //@@TODO GET editor.parsedObject from a function!!
+    const jsonObject = getState().editor.parsedObject
+    mock.updateMock(state.id,state.manageKey, ramlContent, jsonObject)
+  }
+}
+
+const addMockBaseUri = (ramlContent, baseUri) => (dispatch) => {
+  const newContent = ramlContent.split('\n').map(line => {
+    if (line.trim().startsWith('baseUri')) {
+      return 'baseUri: ' + baseUri + " # " + line
+    } else return line
+  }).join('\n')
+  dispatch(updateCurrentFile(newContent))
+}
+
+const removeMockBaseUri = (ramlContent, baseUri) => (dispatch) => {
+  const newContent = ramlContent.split('\n').map(line => {
+    if (line.trim().startsWith('baseUri')) {
+      return line.replace('baseUri: ' + baseUri + ' # ', '')
+    } else return line
+  }).join('\n')
+  dispatch(updateCurrentFile(newContent))
+}
+
 export const deleteMock = ()  => (dispatch, getState) => {
   if (getState().mock.isUp && !getState().mock.isStopping) {
+    const baseUri = getState().mock.baseUri
     dispatch(beginStopMock)
     const mock = new MockingService(new MockingServiceClient())
     mock.deleteMock(getState().mock.id, getState().mock.manageKey).then(() => {
       dispatch(stopMock)
+      const ramlContent = getCurrentFileContent(getState())()
+      removeMockBaseUri(ramlContent, baseUri)(dispatch)
     }).catch(err => {
       console.log(err)
       dispatch(stopMock)
